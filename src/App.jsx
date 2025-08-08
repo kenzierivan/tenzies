99b921ed7f92ac46acfbc2b7deed9bbeb0d1b033
Kenzie Rivan <kenzierivan261@gmail.com>
2025-08-08 15:15:32 +0800

    Create dices and add styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,27 @@
+import { useState } from "react";
+import Die from "./components/Die"
+
 const App = () => {
+
+  const [dice, setDice] = useState(generateAllNewDice());
+
+  function generateAllNewDice() {
+    const newDice = [];
+    for(let i=0; i<10; i++) {
+      newDice.push(Math.floor(Math.random() * 6) + 1);
+    }
+    return newDice;
+  }
+
+  const listOfDice = dice.map((die, index) => {
+    return <Die key={index} number={die} />
+  })
   return (
-    <div>App</div>
+    <main>
+      <div className="dice-container">
+        {listOfDice}
+      </div>
+    </main>
   )
 }
 export default App
---
+++ src/components/Die.jsx
@@ -0,0 +1,6 @@
+const Die = (props) => {
+  return (
+    <button className="die">{props.number}</button>
+  )
+}
+export default Die
